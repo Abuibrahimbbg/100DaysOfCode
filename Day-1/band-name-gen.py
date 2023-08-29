6393982c9713ad211545ff2662ed9910241cb2c7
Abuibrahimbbg <abuibrahimbbg@gmail.com>
2023-08-29 19:16:31 +0100

    Day 1 project - Band name generator

---
+++ Day-1/band-name-gen.py
@@ -0,0 +1,9 @@
+# A band name generator program
+# A welcome message
+print("Welcom to Band Name Generator!\nFollow the instructions to create your band name")
+#get user input (city name) and store in city variable
+city = input("Which city did you grow up in?\n")
+#get user input (favorite pet's name) and store in fav_pet variable
+fav_pet = input("What is your favorite pet's name?\n")
+#print brand name by concatenating the city and fav_pet variable
+print("Your band name could be " + city + " " + fav_pet)
